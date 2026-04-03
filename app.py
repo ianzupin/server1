c69bd4dc77921f24896ffa6c6333ed3031255306
ianzupin <ianzupin@gmail.com>
2026-04-03 20:41:06 +0200

    third upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, send
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
